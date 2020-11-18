c2513892a882f5d645d76a87322d793b764e97b0
Serhii Boiko <boyko0043@gmail.com>
2020-11-18 19:17:39 +0200

    v1

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,13 @@
-$(function(){
+$(function () {
 
+    $('.slider').slick({
+        prevArrow: '',
+        nextArrow: '',
+        infinite: true,
+        dots: true,
+        speed: 1000,
+        autoplay: true,
+        autoplaySpeed: 3000,
+    });
 
-
-  
 });
