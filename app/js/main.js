b97e0566d7fabef104dfcb003ff300f84218a648
Boiko Serhii <boyko0043@gmail.com>
2020-11-19 08:54:23 +0200

    v1

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,8 @@
 $(function () {
 
     $('.slider').slick({
-        prevArrow: '',
-        nextArrow: '',
+        prevArrow: '<button class="banner__slider-btn"><img class="banner__img" src="./images/arrow-left.png" alt="arrow-left" ></button>',
+        nextArrow: '<button class="banner__slider-btn"><img class="banner__img" src="./images/arrow-right.png" alt="arrow-right" ></button>',
         infinite: true,
         dots: true,
         speed: 1000,
